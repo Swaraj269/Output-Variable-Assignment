ea40d011daa24a4fc1f191b0d2fce09a620287eb
Swaraj Singh <149864567+Swaraj269@users.noreply.github.com>
2024-05-19 13:10:27 +0530

    Assignment-1 Done

---
+++ Assignment - 1/question1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "Physics\nWallah";
+    cout<< endl;
+    // another method 
+    cout<< "Physics"<< endl << "Wallah";
+    return 0;
+}
---
+++ Assignment - 1/question5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int radius = 10;
+    float circumference = 2*3.14*radius;
+    cout<< "The circumference of the circle is : "<< circumference;
+    return 0;
+}
